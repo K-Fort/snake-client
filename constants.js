037b68c7376db32a18fa7c69934a6cf70698c4a9
K-Fort <fortink01@gmail.com>
2024-07-04 17:11:36 -0400

    constant file

---
+++ constants.js
@@ -0,0 +1,9 @@
+const IP = 'localhost';
+const PORT = 50541;
+
+
+module.exports = IP, PORT
+
+
+// This file is made of constants for strings and other data that are "static" in our code. We define these values all in one configuration file so that the app can be easily tweaked.
+
